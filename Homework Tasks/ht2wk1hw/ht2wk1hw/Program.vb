01eebaeab624026368606d7beb9bbc29561f27b5
Jack White <jw150908@gmail.com>
2025-11-03 19:36:14 +0000

    finished ht2wk1hw - sum of squares

--- Homework Tasks/ht2wk1hw/ht2wk1hw/Program.vb
+++ Homework Tasks/ht2wk1hw/ht2wk1hw/Program.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-
+        Console.WriteLine(squareOfSum(100, 0) - sumOfSquares(100, 0))
     End Sub
 
     Function sumOfSquares(maxNum As Integer, total As Integer) As Integer
@@ -11,6 +11,16 @@ Module Program
         Else
             total += (maxNum ^ 2)
             Return sumOfSquares(maxNum - 1, total)
+        End If
+    End Function
+
+    Function squareOfSum(maxNum As Integer, total As Integer) As Integer
+        If maxNum = 0 Then
+            total *= total
+            Return total
+        Else
+            total += maxNum
+            Return squareOfSum(maxNum - 1, total)
         End If
     End Function
 End Module
