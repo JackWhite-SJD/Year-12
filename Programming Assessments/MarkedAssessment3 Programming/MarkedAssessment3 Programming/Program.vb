8e2810a034c16fe0d71473f2ccb556918c15a824
jack white <25028699@sjd.ac.uk>
2025-11-27 14:47:16 +0000

    marked assessment3 programming work

---
+++ Programming Assessments/MarkedAssessment3 Programming/MarkedAssessment3 Programming/Program.vb
@@ -0,0 +1,29 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim input As String
+        Dim choice() As String
+        Dim num1 As String
+        Dim num2 As String
+        Dim total As Decimal
+
+
+        Console.WriteLine("Enter inputted unsigned binary.")
+        input = Console.ReadLine()
+        choice = input.Split(".")
+        num1 = StrReverse(choice(0))
+        num2 = choice(1)
+
+        For i = 3 To 0 Step -1
+            If num1(i) = "1" Then
+                total += (2 ^ (i))
+            End If
+            If num2(i) = "1" Then
+                total += 1 / ((2 ^ (i + 1)))
+            End If
+        Next
+
+        Console.WriteLine(total)
+    End Sub
+End Module
