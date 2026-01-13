767978e1e7cd1c7f9df366bfcfd85ad186beee27
jack white <25028699@sjd.ac.uk>
2026-01-13 11:11:26 +0000

    finished animals example

--- Lesson Tasks/INTRO TO OOP/INTRO TO OOP/Program.vb
+++ Lesson Tasks/INTRO TO OOP/INTRO TO OOP/Program.vb
@@ -1,7 +1,41 @@
 Imports System
 
 Module Program
+    '   -----------------------------------------------------------------
+    '  | abstraction by generalisation:                                  |  
+    '  |    grouping common characteristics to arrive at a hierarchical  |
+    '  |    relationship of the "is a kind of" type                      |
+    '   -----------------------------------------------------------------
+    Class Animal
+        Private _strName As String
+        Private _intAge As Integer
+
+        Public Sub setName(ByVal value As String)
+            _strName = value
+        End Sub
+
+        Public Function getName()
+            Return _strName
+        End Function
+
+        Public Overridable Sub makeNoise()
+            Console.WriteLine("The animal has made a noise")
+        End Sub
+
+    End Class
+
+    Class Penguin
+        Inherits Animal ' child class of Animal, can access properties and methds of Animal.
+
+        Public Overrides Sub makeNoise() 'process called polymorphism.
+            Console.WriteLine("Noot Noot")
+        End Sub
+
+    End Class
+
+
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim penguin As New Penguin
+        penguin.setName("Pingu")
     End Sub
 End Module
