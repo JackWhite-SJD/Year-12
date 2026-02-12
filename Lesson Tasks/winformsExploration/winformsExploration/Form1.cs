ba9aa2bd775ffca36e969db55a32c4458aa1d4ab
jack white <25028699@sjd.ac.uk>
2026-02-12 15:13:10 +0000

    intialformscommit

---
+++ Lesson Tasks/winformsExploration/winformsExploration/Form1.cs
@@ -0,0 +1,30 @@
+namespace winformsExploration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<Button> buttons = new List<Button>();
+            buttons.Add(button);
+            buttons.Add(button2);
+            buttons.Add(button3);
+            buttons.Add(button4);
+            buttons.Add(button5);
+            buttons.Add(button6);
+            buttons.Add(button7);
+            buttons.Add(button8);
+            buttons.Add(button9);
+            buttons.Add(button10);
+            buttons.Add(button11);
+            buttons.Add(button12);
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
