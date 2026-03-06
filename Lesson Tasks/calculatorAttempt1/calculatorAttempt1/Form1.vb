7c013cd88acf7919a9e06bee3231ecb655ec8026
jack white <25028699@sjd.ac.uk>
2026-03-06 10:05:50 +0000

    endOf lesson1 Calculator Commit for vb winforms

--- Lesson Tasks/calculatorAttempt1/calculatorAttempt1/Form1.vb
+++ Lesson Tasks/calculatorAttempt1/calculatorAttempt1/Form1.vb
@@ -1,56 +1,73 @@
 ﻿Public Class Form1
-
-    Dim operatorPlus As New Button
-    Dim operatorMinus As New Button
-    Dim operatorTimes As New Button
-    Dim operatorDivide As New Button
-    Dim operatorEquals As New Button
-    Dim operatorDecimal As New Button
     Dim boxEqn As New TextBox
 
-    Dim listOfButtons As List(Of Button)
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
-        placeButton(operatorPlus, 30, 50, "+")
-        placeButton(operatorMinus, 120, 50, "-")
-        placeButton(operatorTimes, 30, 100, "*")
-        placeButton(operatorDivide, 120, 100, "÷")
-        placeButton(operatorEquals, 210, 150, "=")
-
+        placeButtons(_initializeButtons)
     End Sub
 
-
-    Public Sub placeButton(btn As Button, left As Integer, top As Integer, text As String)
-        With btn
-            .Left = left
-            .Top = top
-            .Text = text
-        End With
-        AddHandler btn.Click, AddressOf Button_Click
-        Me.Controls.Add(btn)
-    End Sub
 
     Public Sub Button_Click(sender As Object, e As EventArgs)
         Dim btn As Button = sender
-        Dim symbol As String = btn.Text
+        btn.Text = "a"
     End Sub
 
     Public Function _initializeButtons() As Button(,)
         Dim btn0 As New Button
+        btn0.Text = "0"
+
         Dim btn1 As New Button
+        btn1.Text = "1"
+
         Dim btn2 As New Button
+        btn2.Text = "2"
+
         Dim btn3 As New Button
+        btn3.Text = "3"
+
         Dim btn4 As New Button
+        btn4.Text = "4"
+
         Dim btn5 As New Button
+        btn5.Text = "5"
+
         Dim btn6 As New Button
+        btn6.Text = "6"
         Dim btn7 As New Button
+        btn7.Text = "7"
         Dim btn8 As New Button
+        btn8.Text = "8"
         Dim btn9 As New Button
+        btn9.Text = "9"
+
+        Dim operatorPlus As New Button
+        Dim operatorMinus As New Button
+        Dim operatorTimes As New Button
+        Dim operatorDivide As New Button
+        Dim operatorEquals As New Button
+        Dim operatorDecimal As New Button
+        Dim operatorAC As New Button
 
 
-        Return {{btn1, btn2, btn3, operatorPlus}, {btn4, btn5, btn6, operatorMinus}, {btn7, btn8, btn9, operatorTimes}, {btn0, operatorDecimal, operatorEquals}}
+        Return {{btn1, btn2, btn3, operatorPlus}, {btn4, btn5, btn6, operatorMinus}, {btn7, btn8, btn9, operatorTimes}, {btn0, operatorDecimal, operatorEquals, operatorAC}}
     End Function
+
+    Public Sub placeButtons(btnArr As Button(,))
+        Dim posX As Integer = 30
+        Dim posY As Integer = 50
+        Dim posXIncrement As Integer = 90
+        Dim posYIncrement As Integer = 50
+
+        For i = 0 To 3
+            For j = 0 To 3
+                With btnArr(i, j)
+                    .Left = posX + (posXIncrement * j)
+                    .Top = posY + (posYIncrement * i)
+                End With
+                Me.Controls.Add(btnArr(i, j))
+                AddHandler btnArr(i, j).Click, AddressOf Button_Click
+            Next
+        Next
+    End Sub
 
 End Class
