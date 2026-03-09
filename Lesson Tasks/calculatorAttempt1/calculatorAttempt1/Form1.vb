b57d47351fb14b9fd3f01ebe0a9f17dc67b45c6b
jack white <25028699@sjd.ac.uk>
2026-03-09 15:28:04 +0000

    l2 for calc attempt1

--- Lesson Tasks/calculatorAttempt1/calculatorAttempt1/Form1.vb
+++ Lesson Tasks/calculatorAttempt1/calculatorAttempt1/Form1.vb
@@ -1,16 +1,109 @@
 ﻿Public Class Form1
     Dim boxEqn As New TextBox
-
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        placeButtons(_initializeButtons)
+    Dim isOperatorUsed As Boolean = False
+    Dim arrOfButtons(,) As Button
+    Dim arrOfOperators() As Button
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        arrOfButtons = _initializeButtons()
+        arrOfOperators = getOperatorButtons(arrOfButtons)
+        placeButtons(arrOfButtons)
+        Me.Controls.Add(boxEqn)
     End Sub
 
 
     Public Sub Button_Click(sender As Object, e As EventArgs)
         Dim btn As Button = sender
-        btn.Text = "a"
+        boxEqn.Text += btn.Text
+
     End Sub
+
+    Public Sub Operator_Click(sender As Object, e As EventArgs)
+        Dim btn As Button = sender
+        'switchOperatorEnabled()
+    End Sub
+
+    Public Sub Equals_Click(sender As Object, e As EventArgs)
+        Dim equation As String = boxEqn.Text
+        Dim operatorSelected As Boolean = False
+        Dim strOperator As String
+        Dim strNum1 As String
+        Dim strNum2 As String
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim result As Decimal
+
+
+        For Each s As String In equation
+            Try
+                Integer.Parse(s)
+                If operatorSelected = False Then
+                    strNum1 += s
+                Else
+                    strNum2 += s
+                End If
+
+            Catch ex As Exception
+                operatorSelected = True
+                strOperator = s
+            End Try
+        Next
+
+        intNum1 = Integer.Parse(strNum1)
+        intNum2 = Integer.Parse(strNum2)
+
+        Select Case strOperator
+            Case "+"
+                result = intNum1 + intNum2
+            Case "-"
+                result = intNum1 - intNum2
+            Case "x"
+                result = intNum1 * intNum2
+            Case "÷"
+                result = Math.Round(intNum1 / intNum2, 2)
+            Case Else
+                result = intNum1
+        End Select
+        boxEqn.Text = result.ToString()
+        AC_Click(sender, e)
+        arrOfButtons(3, 1).Text = result.ToString
+    End Sub
+
+    'Public Sub switchOperatorEnabled()
+    '    If operatorUsed = False Then
+    '        operatorUsed = True
+    '    Else
+    '        operatorUsed = False
+    '    End If
+    '    For i = 0 To arrOfOperators(i)
+    '        If arrOfOperators(i).Text = "=" Or arrOfOperators(i).Text = "AC" Or arrOfOperators(i).Text = "." Then
+    '            Continue For
+    '        End If
+    '        arrOfOperators(i).Enabled = operatorUsed
+    '    Next
+    'End Sub
+
+    Public Sub AC_Click(sender As Object, e As EventArgs)
+        boxEqn.Text = ""
+    End Sub
+
+    Public Sub Decimal_click(sender As Object, e As EventArgs)
+        Dim btn As Button = sender
+        boxEqn.Text += btn.Text
+    End Sub
+
+    Public Function getOperatorButtons(btnArr(,) As Button) As Button()
+        Dim newArr(6) As Button
+        For i = 0 To 3
+            For j = 0 To 3
+                Try
+                    Integer.Parse(btnArr(i, j).Text)
+                Catch ex As Exception
+                    newArr.Append(btnArr(i, j))
+                End Try
+            Next
+        Next
+        Return newArr
+    End Function
 
     Public Function _initializeButtons() As Button(,)
         Dim btn0 As New Button
@@ -33,21 +126,43 @@
 
         Dim btn6 As New Button
         btn6.Text = "6"
+
         Dim btn7 As New Button
         btn7.Text = "7"
+
         Dim btn8 As New Button
         btn8.Text = "8"
+
         Dim btn9 As New Button
         btn9.Text = "9"
 
         Dim operatorPlus As New Button
-        Dim operatorMinus As New Button
-        Dim operatorTimes As New Button
-        Dim operatorDivide As New Button
-        Dim operatorEquals As New Button
-        Dim operatorDecimal As New Button
-        Dim operatorAC As New Button
+        operatorPlus.Text = "+"
+        AddHandler operatorPlus.Click, AddressOf Operator_Click
 
+        Dim operatorMinus As New Button
+        operatorMinus.Text = "-"
+        AddHandler operatorMinus.Click, AddressOf Operator_Click
+
+        Dim operatorTimes As New Button
+        operatorTimes.Text = "x"
+        AddHandler operatorTimes.Click, AddressOf Operator_Click
+
+        Dim operatorDivide As New Button
+        operatorDivide.Text = "÷"
+        AddHandler operatorDivide.Click, AddressOf Operator_Click
+
+        Dim operatorEquals As New Button
+        operatorEquals.Text = "="
+        AddHandler operatorEquals.Click, AddressOf Equals_Click
+
+        Dim operatorDecimal As New Button
+        operatorDecimal.Text = "."
+        AddHandler operatorDecimal.Click, AddressOf Decimal_click
+
+        Dim operatorAC As New Button
+        operatorAC.Text = "AC"
+        AddHandler operatorAC.Click, AddressOf AC_Click
 
         Return {{btn1, btn2, btn3, operatorPlus}, {btn4, btn5, btn6, operatorMinus}, {btn7, btn8, btn9, operatorTimes}, {btn0, operatorDecimal, operatorEquals, operatorAC}}
     End Function
@@ -65,7 +180,11 @@
                     .Top = posY + (posYIncrement * i)
                 End With
                 Me.Controls.Add(btnArr(i, j))
-                AddHandler btnArr(i, j).Click, AddressOf Button_Click
+                If btnArr(i, j).Text = "AC" Or btnArr(i, j).Text = "=" Then
+                    Continue For
+                Else
+                    AddHandler btnArr(i, j).Click, AddressOf Button_Click
+                End If
             Next
         Next
     End Sub
