9f5a6e36287b65202274139f89b019413a7defcf
jack white <25028699@sjd.ac.uk>
2026-03-20 09:33:31 +0000

    initial saving and loading csvs commit

---
+++ Lesson Tasks/savingAndLoadingCSVs/savingAndLoadingCSVs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+namespace savingAndLoadingCSVs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            csvWriter();
+            csvReader();
+        }
+
+        public static void csvWriter()
+        {
+            StreamWriter fileWriter = new StreamWriter("test.csv",true);
+            fileWriter.WriteLine("Adam,Cheese,26");
+            string filePath = Path.GetFullPath("test.csv");
+            Console.WriteLine(filePath);
+            fileWriter.Close();
+        }
+
+        public static void csvReader()
+        {
+            string currentline = "";
+            try
+            {
+                StreamReader fileReader = new StreamReader("test.csv");
+                while (fileReader.Peek() != -1)
+                {
+                    currentline = fileReader.ReadLine();
+                    string[] currentRecord = currentline.Split(",");
+                    Console.WriteLine(currentRecord[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
